2aa79ba500f46814f3d5c783bbcd0e447f2a7d3e
whale <whalefc97@mf.me>
2023-11-08 11:35:28 -0600

    more functions

--- src/main/java/sh/cocaine/sexo/Listener.java
+++ src/main/java/sh/cocaine/sexo/Listener.java
@@ -14,11 +14,11 @@ import java.util.Objects;
 public class Listener extends ListenerAdapter {
 
     HashMap<String, BotUser> userList = new HashMap<>() {{
-        put("whale@snow.cocaine.sh", new BotUser("whale@snow.cocaine.sh", (byte) 3, true, false));
-        put("horse@pony.equus.sh", new BotUser("horse@pony.equus.sh", (byte) 1, true, false));
-        put("no@50.47.219.16", new BotUser("no@50.47.219.16", (byte) 1, true, false));
-        put("kr@m3r.sh", new BotUser("kr@m3r.sh", (byte) 1, true, false));
-        put("sigmakitty@hammond.expi.pl", new BotUser("sigmakitty@hammond.expi.pl", (byte) 1, true, false));
+        put("whale@snow.cocaine.sh", new BotUser("whale@snow.cocaine.sh", (byte) 2, true, false));
+        put("horse@pony.equus.sh", new BotUser("horse@pony.equus.sh", (byte) 2, true, false));
+        put("no@50.47.219.16", new BotUser("no@50.47.219.16", (byte) 2, true, false));
+        put("kr@m3r.sh", new BotUser("kr@m3r.sh", (byte) 2, true, false));
+        put("sigmakitty@hammond.expi.pl", new BotUser("sigmakitty@hammond.expi.pl", (byte) 2, true, false));
         put("dolphin@static.191.75.78.5.clients.your-server.de", new BotUser("dolphin@static.191.75.78.5.clients.your-server.de", (byte) 0, false, true));
     }};
 
@@ -44,6 +44,12 @@ public class Listener extends ListenerAdapter {
         if (event.getMessage().startsWith("!")) {
             String[] args = event.getMessage().split(" ");
 
+            // create a hashmap of the users in channel and their hostmasks
+            HashMap<String, String> channelUsers = new HashMap<String, String>();
+            for (User user : event.getChannel().getUsers()) {
+                channelUsers.put(user.getNick().toLowerCase(), user.getHostmask());
+            }
+
             if (args[0].equals("!op") && isLevelOp(eventHostmask)) {
                 if (args.length == 1) {
                     event.getChannel().send().setMode("+o " + Objects.requireNonNull(event.getUser()).getNick());
@@ -57,26 +63,74 @@ public class Listener extends ListenerAdapter {
                 }
             }
 
+            if(args[0].equals("!voice") && isLevelOp(eventHostmask)) {
+                if (args.length == 1) {
+                    event.getChannel().send().setMode("+v " + Objects.requireNonNull(event.getUser()).getNick());
+                    System.out.println("Gave " + event.getUser().getNick() + " voice");
+                }
+                if (args.length == 2) {
+                    event.getChannel().send().setMode("+v " + args[1]);
+                    System.out.println("Gave " + args[1] + " voice on behalf of " + Objects.requireNonNull(event.getUser()).getNick());
+                } else {
+                    event.getChannel().send().message("Usage: !voice <nick>");
+                }
+            }
+
             if (args[0].equals("!addop") && isLevelAdmin(eventHostmask)) {
                 if (args.length == 2) {
-                    // create a hashmap of the users in channel and their hostmasks
-                    HashMap<String, String> channelUsers = new HashMap<String, String>();
-                    for (User user : event.getChannel().getUsers()) {
-                        channelUsers.put(user.getNick().toLowerCase(), user.getHostmask());
-                    }
 
                     // if userList doesn't contain the hostmask, add it
-                    if (!userList.containsKey(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
-                        userList.put(filteredHostmask(channelUsers.get(args[1].toLowerCase())),
-                                new BotUser(filteredHostmask(channelUsers.get(args[1].toLowerCase())), (byte) 1, true, false));
+                    if (!doesBotUserExist(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
+                        addBotUser(filteredHostmask(channelUsers.get(args[1].toLowerCase())), (byte) 1, true, false);
 
                         event.getChannel().send().message("Added " + args[1] + " to the op list");
                         System.out.println("Added " + args[1] + " to the op list");
                     } else {
                         System.out.println(args[1] + " already exists in the op list.");
+                        if(isLevelUser(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
+                            setUserLevel(filteredHostmask(channelUsers.get(args[1].toLowerCase())), (byte) 1);
+                        }
+                        setAutoOp(filteredHostmask(channelUsers.get(args[1].toLowerCase())), true);
                     }
                 } else {
                     event.getChannel().send().message("Usage: !addop <nick>");
+                }
+            }
+
+            if (args[0].equals("!addvoice") && isLevelOp(eventHostmask)) {
+                if (args.length == 2) {
+
+                    // if userList doesn't contain the hostmask, add it
+                    if (!doesBotUserExist(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
+                        addBotUser(filteredHostmask(channelUsers.get(args[1].toLowerCase())), (byte) 0, false, true);
+
+                        event.getChannel().send().message("Added " + args[1] + " to the voice list");
+                        System.out.println("Added " + args[1] + " to the voice list");
+                    } else {
+                        System.out.println(args[1] + " already exists in the voice list.");
+                        if(isLevelUser(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
+                            setUserLevel(filteredHostmask(channelUsers.get(args[1].toLowerCase())), (byte) 0);
+                        }
+                        setAutoVoice(filteredHostmask(channelUsers.get(args[1].toLowerCase())), true);
+                    }
+                } else {
+                    event.getChannel().send().message("Usage: !addvoice <nick>");
+                }
+            }
+
+            if (args[0].equals("!removeBotUser") && isLevelAdmin(eventHostmask)) {
+                if (args.length == 2) {
+                    if (doesBotUserExist(filteredHostmask(channelUsers.get(args[1].toLowerCase())))) {
+                        userList.remove(filteredHostmask(channelUsers.get(args[1].toLowerCase())));
+
+                        event.getChannel().send().message("Removed " + args[1] + " from the bot user list");
+                        System.out.println("Removed " + args[1] + " from the bot user list");
+                    } else {
+                        event.getChannel().send().message(args[1] + " does not exist in the bot user list");
+                        System.out.println(args[1] + " does not exist in the bot user list");
+                    }
+                } else {
+                    event.getChannel().send().message("Usage: !removeBotUser <nick>");
                 }
             }
         }
@@ -101,6 +155,14 @@ public class Listener extends ListenerAdapter {
         }
     }
 
+    public boolean isLevelUser(String hostmask) {
+        if (userList.containsKey(filteredHostmask(hostmask))) {
+            return (userList.get(filteredHostmask(hostmask))).level == 0;
+        } else {
+            return false;
+        }
+    }
+
     public boolean isLevelOp(String hostmask) {
         if (userList.containsKey(filteredHostmask(hostmask))) {
             return (userList.get(filteredHostmask(hostmask))).level >= 1;
@@ -110,9 +172,35 @@ public class Listener extends ListenerAdapter {
     }
     public boolean isLevelAdmin(String hostmask) {
         if (userList.containsKey(filteredHostmask(hostmask))) {
-            return (userList.get(filteredHostmask(hostmask))).level >= 3;
+            return (userList.get(filteredHostmask(hostmask))).level >= 2;
         } else {
             return false;
+        }
+    }
+
+    public boolean doesBotUserExist(String hostmask) {
+        return userList.containsKey(filteredHostmask(hostmask));
+    }
+
+    public void addBotUser(String hostmask, byte level, boolean autoOp, boolean autoVoice) {
+        userList.put(hostmask, new BotUser(hostmask, level, autoOp, autoVoice));
+    }
+
+    public void setUserLevel(String hostmask, byte level) {
+        if (userList.containsKey(filteredHostmask(hostmask))) {
+            userList.get(filteredHostmask(hostmask)).level = level;
+        }
+    }
+
+    public void setAutoOp(String hostmask, boolean autoOp) {
+        if (userList.containsKey(filteredHostmask(hostmask))) {
+            userList.get(filteredHostmask(hostmask)).autoOp = autoOp;
+        }
+    }
+
+    public void setAutoVoice(String hostmask, boolean autoVoice) {
+        if (userList.containsKey(filteredHostmask(hostmask))) {
+            userList.get(filteredHostmask(hostmask)).autoVoice = autoVoice;
         }
     }
 
